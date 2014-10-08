b0e74e8ebcaf992d85e3e685aab85debd979d01a
Chris Gomez <chris_gomez@bitovi.com>
2014-10-07 23:30:29 -0400

    Hide nav-collapse on blur

--- public/src/component/header/header.js
+++ public/src/component/header/header.js
@@ -10,8 +10,35 @@ function (can, template) {
       init: function () {
 
       },
-      inserted: function () {
 
+      inserted: function () {
+        this.onDocumentClick = can.proxy(this.blurNavCollapse, this);
+      },
+
+      '.navbar-collapse show.bs.collapse': function () {
+        $(document).on('click', this.onDocumentClick);
+      },
+
+      '.navbar-collapse hide.bs.collapse': function () {
+        $(document).off('click', this.onDocumentClick);
+      },
+
+      '.navbar-collapse a click': function () {
+        this.hideNavCollapse()
+      },
+
+      hideNavCollapse: function () {
+        this.element.find('.navbar-collapse').collapse('hide');
+      },
+
+      blurNavCollapse: function (ev) {
+        var tagName = this.element.prop('tagName').toLowerCase();
+        var clickedEl = ev.target;
+        var isChildOfHeader = $(clickedEl).parents(tagName).length;
+
+        if (! isChildOfHeader) {
+          this.hideNavCollapse();
+        }
       }
     }
   });
