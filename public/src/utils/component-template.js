a98ba4b9f3ffd52084fff69aa15495889d0224c9
Chris Gomez <chris_gomez@bitovi.com>
2014-10-14 09:42:10 -0400

    Add ViewModel to template

--- public/src/utils/component-template.js
+++ public/src/utils/component-template.js
@@ -1,6 +1,13 @@
 steal('can',
   './template.stache!',
+  'can/map/define',
   function (can, template) {
+
+  var ViewModel = can.Map.extend({
+    define: {
+
+    }
+  });
 
   return can.Component.extend({
     tag: 'app-',
